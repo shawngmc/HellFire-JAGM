93250753ebe6b1fb02aede07d3cab6958a64b190
shawngmc <7342147+shawngmc@users.noreply.github.com>
2025-09-11 19:08:09 -0400

    Add default performance settings for browser prefs

---
+++ prefs/additional.js
@@ -0,0 +1,4 @@
+// Default to HW Acceleration Enabled
+pref("browser.preferences.defaultPerformanceSettings.enabled", false);
+pref("layers.acceleration.disabled", false);
+pref("layers.acceleration.force-Enabled", true);
